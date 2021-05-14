308e078d128eb093efda18430112c0e1a53fa398
Justin Ng <ng.justin07@gmail.com>
2021-05-13 21:10:53 -0400

    fixed logout and debugging login

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controller');
 const path = require('path');
+const helpers=require('./utils/helpers')
 
 require('dotenv').config();
 
