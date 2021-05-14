b303b9a01780e5657a2077adf11c8978b19a1a5d
Justin Ng <ng.justin07@gmail.com>
2021-05-13 20:09:16 -0400

    added saving feature/login-routes/updatedhandlebars

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({})
+const hbs = exphbs.create({helpers})
 const app = express();
 const PORT = process.env.PORT || 3001;
 
